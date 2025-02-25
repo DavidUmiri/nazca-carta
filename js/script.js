3c10a1cd791749568e8e1879ca4122e261c2a165
DavidUmiri <davidumiri@gmail.com>
2025-02-25 20:18:18 +0100

    Loading all pages

--- js/script.js
+++ js/script.js
@@ -3,37 +3,41 @@ const container = document.getElementById('pdf-container');
 const pageSize = 5; // Número de páginas a cargar de manera incremental
 
 pdfjsLib.getDocument(url).promise.then(pdf => {
-    const totalPages = pdf.numPages;
-    let currentPage = 1;
+  const totalPages = pdf.numPages;
+  let currentPage = 1;
 
-    function loadNextPages() {
-        for (let i = currentPage; i <= Math.min(currentPage + pageSize - 1, totalPages); i++) {
-            pdf.getPage(i).then(page => {
-                const scale = container.clientWidth / page.getViewport({ scale: 1 }).width;
-                const viewport = page.getViewport({ scale: scale });
+  function loadNextPages() {
+    for (let i = currentPage; i <= Math.min(currentPage + pageSize - 1, totalPages); i++) {
+      pdf.getPage(i).then(page => {
+        const scale = container.clientWidth / page.getViewport({ scale: 1 }).width;
+        const viewport = page.getViewport({ scale: scale });
 
-                const canvas = document.createElement('canvas');
-                canvas.style.marginBottom = '20px'; // Opcional: espacio entre páginas
-                container.appendChild(canvas);
+        const canvas = document.createElement('canvas');
+        container.appendChild(canvas);
 
-                const context = canvas.getContext('2d');
-                canvas.height = viewport.height;
-                canvas.width = viewport.width;
+        const context = canvas.getContext('2d');
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
 
-                page.render({ canvasContext: context, viewport: viewport });
-            });
-        }
-        currentPage += pageSize;
+        page.render({ canvasContext: context, viewport: viewport });
+      });
     }
+    currentPage += pageSize;
 
-    loadNextPages();
+    // Verificar si hay más páginas después de la página actual
+    if (currentPage <= totalPages) {
+      loadNextPages();
+    }
+  }
 
-    // Agregar un evento de scroll para cargar las siguientes páginas
-    container.addEventListener('scroll', () => {
-        if (container.scrollTop + container.offsetHeight >= container.scrollHeight) {
-            if (currentPage <= totalPages) {
-                loadNextPages();
-            }
-        }
-    });
+  loadNextPages();
+
+  // Agregar un evento de scroll para cargar las siguientes páginas
+  container.addEventListener('scroll', () => {
+    if (container.scrollTop + container.offsetHeight >= container.scrollHeight) {
+      if (currentPage <= totalPages) {
+        loadNextPages();
+      }
+    }
+  });
 });
